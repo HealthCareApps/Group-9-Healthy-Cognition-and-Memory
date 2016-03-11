92b466d617e882aeb5e0baeb85b43eba91858d53
ssmodish <smodish@hotmail.com>
2016-03-11 15:32:07 -0500

    Quiz5
    Code Submitted by Khalil for Quiz 5

---
+++ HealtyThinkingProject/HealthyThinking/app/src/main/java/edu/fau/kmillwood/healthythinking/IdentifyingFragment.java
@@ -0,0 +1,34 @@
+package edu.fau.kmillwood.healthythinking;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IdentifyingFragment extends Fragment {
+
+
+    public IdentifyingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+
+        return inflater.inflate(R.layout.fragment_identifying, container,false);
+    }
+
+}
